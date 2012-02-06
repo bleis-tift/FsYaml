2eac020dc3d2ffc6402655ab55d544082dfc6d7d
hafuu <hafuu46@gmail.com>
2012-02-06 16:36:55 +0900

    stringのdump refs #3

---
+++ FsYaml.Tests/YamlDumpTests.fs
@@ -0,0 +1,20 @@
+﻿module YamlDumpTests
+
+open NUnit.Framework
+open NaturalSpec
+
+[<TestFixture>]
+module StringDump =
+  let DumpCases =
+    TestWith (doubleParam "あいうえお" "あいうえお")
+    |> And (doubleParam "かきくけこ" "かきくけこ")
+    |> And (doubleParam "かきくけ\"こ" "\"かきくけ\\\"こ\"")
+    |> And (doubleParam "かきく\r\nけこ" "\"かきく\r\nけこ\"")
+    |> And (doubleParam "かきく\bけこ" "\"かきく\bけこ\"")
+
+  [<ScenarioSource "DumpCases">]
+  let test (input: string) (expected: string) =
+    Given input
+    |> When Yaml.dump
+    |> It should equal expected
+    |> Verify
