5c3be22bbb315ba37e503f82391298b5e2f67dcf
hafuu <hafuu46@gmail.com>
2012-02-06 17:03:51 +0900

    数値のdump refs #3

--- FsYaml.Tests/YamlDumpTests.fs
+++ FsYaml.Tests/YamlDumpTests.fs
@@ -5,15 +5,63 @@ open NaturalSpec
 
 [<TestFixture>]
 module StringDump =
-  let DumpCases =
+  let Cases =
     TestWith (doubleParam "あいうえお" "あいうえお")
     |> And (doubleParam "かきくけこ" "かきくけこ")
     |> And (doubleParam "かきくけ\"こ" "\"かきくけ\\\"こ\"")
     |> And (doubleParam "かきく\r\nけこ" "\"かきく\r\nけこ\"")
     |> And (doubleParam "かきく\bけこ" "\"かきく\bけこ\"")
 
-  [<ScenarioSource "DumpCases">]
+  [<ScenarioSource "Cases">]
   let test (input: string) (expected: string) =
+    Given input
+    |> When Yaml.dump
+    |> It should equal expected
+    |> Verify
+
+[<TestFixture>]
+module IntDump =
+  let Cases =
+    TestWith (doubleParam 1 "1")
+    |> And (doubleParam 0 "0")
+    |> And (doubleParam -56 "-56")
+    |> And (doubleParam 3000 "3000")
+
+  [<ScenarioSource "Cases">]
+  let test (input: int) (expected: string) =
+    Given input
+    |> When Yaml.dump
+    |> It should equal expected
+    |> Verify
+
+[<TestFixture>]
+module FloatDump =
+  let Cases =
+    TestWith (doubleParam 1.5 "1.5")
+    |> And (doubleParam 30.5 "30.5")
+    |> And (doubleParam 0 "0")
+    |> And (doubleParam -2.5 "-2.5")
+    |> And (doubleParam infinity ".inf")
+    |> And (doubleParam -infinity "-.inf")
+    |> And (doubleParam nan ".nan")
+
+  [<ScenarioSource "Cases">]
+  let test (input: float) (expected: string) =
+    Given input
+    |> When Yaml.dump
+    |> It should equal expected
+    |> Verify
+
+[<TestFixture>]
+module DecimalDump =
+  let Cases =
+    TestWith (doubleParam 1.5m "1.5")
+    |> And (doubleParam 30.6m "30.6")
+    |> And (doubleParam 0m "0")
+    |> And (doubleParam -2.8m "-2.8")
+
+  [<ScenarioSource "Cases">]
+  let test (input: decimal) (expected: string) =
     Given input
     |> When Yaml.dump
     |> It should equal expected
--- FsYaml/YamlDump.fs
+++ FsYaml/YamlDump.fs
@@ -3,6 +3,8 @@
 open System.Text.RegularExpressions
 open System.Text
 
+open Patterns
+
 let dumpString str =
   let pat = @"[\b\r\n\t""]"
   if(Regex.IsMatch(str, pat)) then
@@ -12,5 +14,28 @@ let dumpString str =
   else
     str
 
-let dump obj = dumpString obj
+let dumpInt i = string i
+
+let dumpFloat f =
+  let (|Infinity|NegativeInfinity|NaN|Float|)f =
+    if System.Double.IsNaN f then NaN
+    else if f = infinity then Infinity
+    else if f = -infinity then NegativeInfinity
+    else Float f
+
+  let nInfinity = -infinity
+  match f with
+  | Infinity -> ".inf"
+  | NegativeInfinity -> "-.inf"
+  | NaN -> ".nan"
+  | Float f -> string f
+
+let dump (o: obj) =
+  let t = o.GetType()
+  match t with
+  | StrType -> dumpString (o :?> string)
+  | IntType -> string o
+  | FloatType -> dumpFloat (o :?> float)
+  | DecimalType -> string o
+  | _ -> failwith "未実装なんですけど"
   
