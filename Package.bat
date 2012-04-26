dc9b492991449fdf7c9e0b70a4cf2b7f43638048
bleis-tift <bleis-tift@hotmail.co.jp>
2012-04-26 22:11:30 +0900

    v1.1.1

---
+++ Package.bat
@@ -0,0 +1,11 @@
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
+rmdir /S /Q bin
+
+rmdir /S /Q FsYaml\obj
+rmdir /S /Q FsYaml\bin
+msbuild FsYaml/FsYaml.fsproj /p:Configuration=Release;Platform=AnyCPU /t:Rebuild
+mkdir bin bin\net4.0
+move FsYaml\bin\Release bin\net4.0
+
+nuget pack Package.nuspec
+move *.nupkg bin\
