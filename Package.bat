c18a078915766108e814b93b6aa3390f2b579c60
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2012-04-27 11:48:40 +0900

    v1.1.2

--- Package.bat
+++ Package.bat
@@ -5,7 +5,7 @@ rmdir /S /Q FsYaml\obj
 rmdir /S /Q FsYaml\bin
 msbuild FsYaml/FsYaml.fsproj /p:Configuration=Release;Platform=AnyCPU /t:Rebuild
 mkdir bin bin\net4.0
-move FsYaml\bin\Release bin\net4.0
+move FsYaml\bin\Release\*.dll bin\net4.0
 
 nuget pack Package.nuspec
 move *.nupkg bin\
