75e2219c7daf756e656fba94d7354f622f9bb7ba
m.hirano <m.hirano@otr-jp.com>
2013-11-29 14:03:37 +0900

    1.1.3

--- Package.bat
+++ Package.bat
@@ -1,11 +1,2 @@
-set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
-rmdir /S /Q bin
-
-rmdir /S /Q FsYaml\obj
-rmdir /S /Q FsYaml\bin
-msbuild FsYaml/FsYaml.fsproj /p:Configuration=Release;Platform=AnyCPU /t:Rebuild
-mkdir bin bin\net4.0
-move FsYaml\bin\Release\*.dll bin\net4.0
-
-nuget pack Package.nuspec
-move *.nupkg bin\
+cd FsYaml
+..\.nuget\NuGet.exe pack FsYaml.fsproj -Symbols -Build
